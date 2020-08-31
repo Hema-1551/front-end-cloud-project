15c668b75aa53c4e6ac3a9a0af4be77f9404ed3f
Hemanani <hemanani543@gmail.com>
2020-08-31 15:12:16 +0530

    finally commited

--- userdata.js
+++ userdata.js
@@ -1,7 +1,7 @@
-var mostCommon=0, lessCommon=0, Serious=0;
-var fever_level=0, cough=0, chills_rigors =0, sore_throat =0, shortnessOfBreath=0;
-var runnyNose=0, headache =0, fatigue=0, lossOfTasteSmell=0;
-var chest_pain, lossOfSpeech, tiredness;
+var mostCommon = 0, lessCommon = 0, Serious = 0;
+var fever_level = 0, cough = 0, chills_rigors = 0, sore_throat = 0, shortnessOfBreath = 0;
+var runnyNose = 0, headache = 0, fatigue = 0, lossOfTasteSmell = 0;
+var chest_pain = 0, lossOfSpeech = 0, tiredness = 0;
 
 
 function storeDetails() {
@@ -47,27 +47,26 @@ function funSub() {
 
 
 function getSymptoms() {
-   
+
     if (document.getElementById('fy').checked)
         fever_level = 1;
     else if (document.getElementById('fn').checked)
         fever_level = 0;
     else
-        fever_level = -1;
+         // fever_level = -1;
 
 
-    // cough
 
-
-    if (document.getElementById('Coughy').checked) {
-        cough = 1;
-    }
-    else if (document.getElementById('Coughn').checked) {
-        cough = 0;
-    }
-    else {
-        cough = -1;
-    }
+//cough
+        if (document.getElementById('Coughy').checked) {
+            cough = 1;
+        }
+        else if (document.getElementById('Coughn').checked) {
+            cough = 0;
+        }
+        else {
+             //cough = -1;
+        }
 
     //chills_rigors
     if (document.getElementById('Chills_y').checked) {
@@ -77,7 +76,7 @@ function getSymptoms() {
         chills_rigors = 0;
     }
     else {
-        chills_rigors = -1;
+        // chills_rigors = -1;
     }
 
     //   sore_throat
@@ -89,7 +88,7 @@ function getSymptoms() {
         sore_throat = 0;
     }
     else {
-        sore_throat = -1;
+        //sore_throat = -1;
     }
 
 
@@ -101,7 +100,7 @@ function getSymptoms() {
         shortnessOfBreath = 0;
     }
     else {
-        shortnessOfBreath = -1;
+        // shortnessOfBreath = -1;
     }
     //runnyNose
 
@@ -112,7 +111,7 @@ function getSymptoms() {
         runnyNose = 0;
     }
     else {
-        runnyNose = -1;
+        // runnyNose = -1;
     }
 
     //headache
@@ -123,7 +122,7 @@ function getSymptoms() {
         headache = 0;
     }
     else {
-        headache = -1;
+        // headache = -1;
     }
 
 
@@ -137,7 +136,7 @@ function getSymptoms() {
         fatigue = 0;
     }
     else {
-        fatigue = -1;
+        //  fatigue = -1;
     }
 
     //lossOfTasteSmell
@@ -149,7 +148,7 @@ function getSymptoms() {
         lossOfTasteSmell = 0;
     }
     else {
-        lossOfTasteSmell = -1;
+        // lossOfTasteSmell = -1;
     }
     //chest pain or pressure
 
@@ -160,7 +159,7 @@ function getSymptoms() {
         chest_pain = 0;
     }
     else {
-        chest_pain = -1;
+        //chest_pain = -1;
     }
 
     //loss of speech or movement -->
@@ -171,72 +170,59 @@ function getSymptoms() {
     else if (document.getElementById('speech_n').checked)
         lossOfSpeech = 0;
     else
-        lossOfSpeech = -1;
+        //lossOfSpeech = -1;
 
 
-    //tiredness
+        //tiredness
 
-    if (document.getElementById('tiredness_y').checked)
-        tiredness = 1;
-    else if (document.getElementById('tiredness_n').checked)
-        tiredness = 0;
-    else
-        tiredness = -1;
+        if (document.getElementById('tiredness_y').checked)
+            tiredness = 1;
+        else if (document.getElementById('tiredness_n').checked)
+            tiredness = 0;
+        else
+            // tiredness = -1;
 
 
-    /// manipulation part
+            /// manipulation part
 
-    if (shortnessOfBreath || chest_pain || lossOfSpeech) {
-        Serious = 1;
-        // alert('entred');
-    }
+            if (shortnessOfBreath || chest_pain || lossOfSpeech) {
+                Serious = 1;
+                // alert('entred');
+            }
 
-    else if (fever_level || cough || tiredness)
-        mostCommon = 1;
+            else if (fever_level || cough || tiredness)
+                mostCommon = 1;
 
-    else
-        lessCommon = 1;
+            else
+                lessCommon = 1;
 
-        /* var mostCommon=0, lessCommon=0, Serious=0;
-var fever_level=0, cough=0, chills_rigors =0, sore_throat =0, shortnessOfBreath=0;
-var runnyNose=0, headache =0, fatigue=0, lossOfTasteSmell=0;
-var chest_pain, lossOfSpeech, tiredness;*/
 
-        localStorage.setItem('fever', fever_level);
-        localStorage.setItem('cough', cough);
-        localStorage.setItem('chills_rigors', chills_rigors);
-        localStorage.setItem('sore_throat', sore_throat);
-        localStorage.setItem('shortnessOfBreath', shortnessOfBreath);
-        localStorage.setItem('runnyNose', runnyNose);
-        localStorage.setItem('headache', headache);
-        localStorage.setItem('fatigue', fatigue);
-        localStorage.setItem('lossOfTasteSmell', lossOfTasteSmell);
-        localStorage.setItem('chest_pain', chest_pain);
-        localStorage.setItem('lossOfSpeech', lossOfSpeech);
-        localStorage.setItem('tiredness', tiredness);
-        localStorage.setItem('mostCommon_g', mostCommon);
-        localStorage.setItem('lessCommon_g', lessCommon);
-        localStorage.setItem('Serious_g', Serious);
+    localStorage.setItem('mostCommon_g', mostCommon);
+    localStorage.setItem('lessCommon_g', lessCommon);
+    localStorage.setItem('Serious_g', Serious);
 
 
 }
 
 function result() {
+    var mostCommon_result = 0, lessCommon_result = 0, serious_result = 0;
+    mostCommon_result = parseInt(localStorage.getItem("mostCommon_g"));
+    lessCommon_result = parseInt(localStorage.getItem("lessCommon_g"));
+    serious_result = parseInt(localStorage.getItem("Serious_g"));
 
-    var mostCommon_result=localStorage.getItem("mostCommon_g");
-    var lessCommon_result=localStorage.getItem("lessCommon_g");
-    var serious_result=localStorage.getItem("Serious_g");
-
-    // alert(mostCommon_result);
-    if (mostCommon) {
-        document.getElementById("result").innerHTML = "You have most common symptoms of covid-19\n so, please go for Covid testing";
+    
+    if (mostCommon_result) {
+        document.getElementById("result").
+        innerHTML = "You have most common symptoms of covid-19\n so, please go for Covid testing";
     }
     else if (serious_result) {
-        document.getElementById("result").innerHTML = "You have most serious symptoms of covid-19\n so, please go for Covid testing and also prevent it by self care";
+        document.getElementById("result").
+        innerHTML = "You have most serious symptoms of covid-19\n so, please go for Covid testing and also prevent it by self care";
 
     }
     else {
-        document.getElementById("result").innerHTML = "You have normal symptoms of covid-19\n so, please take self care";
+        document.getElementById("result").
+        innerHTML = "You have No symptoms \n so, please take self care";
 
     }
 }
